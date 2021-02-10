526a3db5af36ba4da075549cea633fb82daa49ba
sergio de luca <sdlbsso@gmail.com>
2021-02-10 13:50:08 -0300

    modificado

--- primer/src/Hola.java
+++ primer/src/Hola.java
@@ -1,5 +1,14 @@
 public class Hola {
+
+    public int sumar(int n1, int n2){
+        return n1+n2;
+    }
+
     public static void main(String[] args) {
         System.out.println("Hola mundillo");
+        int n1 = 5;
+        int n2 = 10;
+
+        System.out.println("La suma de "+n1+ " mas "+n2+ " es = "+ String.valueOf(n1+n2));
     }
 }
